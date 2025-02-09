ab5b4ec109fc690f55a94adda8ae580239027b60
Arsany-Gergs <rsngerges@gmail.com>
2025-02-09 17:36:32 +0200

    add Apartment controller, repository

--- RentApp.Infrastructure/obj/Debug/net8.0/RentApp.Infrastructure.AssemblyInfo.cs
+++ RentApp.Infrastructure/obj/Debug/net8.0/RentApp.Infrastructure.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("RentApp.Infrastructure")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e17a8345f5636463f79e390c7cff8e877ffab956")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1ddf4c43860ba01bf6b1719e124175d65b8fff73")]
 [assembly: System.Reflection.AssemblyProductAttribute("RentApp.Infrastructure")]
 [assembly: System.Reflection.AssemblyTitleAttribute("RentApp.Infrastructure")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
