6452b2195a4dfee25c95ca67911be6fab00ceba9
Arsany-Gergs <rsngerges@gmail.com>
2025-02-24 13:29:50 +0200

    Adding connection to MonsterAsp.Net Database hosting

---
+++ RentApp.Infrastructure/Migrations/20250224111831_init create on Local.Arsany.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace RentApp.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class initcreateonLocalArsany : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
